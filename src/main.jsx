10e65644aaed7e22709f5e48a66143d7ca225f5f
helm808 <45582981+helm808@users.noreply.github.com>
2026-06-11 20:47:52 -0500

    add main.jsx entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NameSign from './NameSign'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NameSign />
+  </React.StrictMode>
+)
